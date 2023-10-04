fc64da873edda9d373568520fa00d1f44ee12146
Elul Asfaha <scorpiondota22@gmail.com>
2023-10-04 22:42:59 +0300

    fixed ui for mobile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,23 +3,25 @@ import work from "../assets/work.png";
 const Home = () => {
     const [email, setEmail] = useState("");
     return (
-        <section className='px-10 max-w-[1400px] min-h-[80vh] flex flex-col justify-center'>
-            <div className='grid grid-cols-2 py-20 gap-10'>
+        <section className='px-5 md:px-10 max-w-[1400px] min-h-[80vh] flex flex-col justify-center'>
+            <div className='grid md:grid-cols-2 py-10 md:py-20 gap-5 md:gap-10'>
                 <div className='flex flex-col justify-center gap-5'>
-                    <p className='text-8xl text-justify'>Autopilot</p>
+                    <p className='text-6xl md:text-8xl text-justify'>
+                        Autopilot
+                    </p>
                     <p className='text-xl font-semibold'>
                         Free up your team from the burdens of creating, manging
                         and finding trusted company information.
                     </p>
-                    <form className='w-full flex'>
+                    <form className='w-full flex flex-col gap-3 md:gap-0 md:flex-row'>
                         <input
                             type='email'
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                             placeholder='name@company.com'
-                            className='flex-1 bg-gray-100 object-cover px-5 outline-none'
+                            className='flex-1 bg-gray-100 object-cover px-5 outline-none py-2'
                         />{" "}
-                        <button className='bg-[#FA000B] w-auto px-5 py-2 font-bold text-white'>
+                        <button className='bg-[#FA000B] md:w-auto px-5 py-2 font-bold text-white'>
                             Sign up for free
                         </button>
                     </form>
@@ -29,53 +31,53 @@ const Home = () => {
                         Ai.
                     </p>
                 </div>
-                <div className='flex h-full w-full pl-16 max-h-[400px]'>
+                <div className='flex h-full w-full md:px-5 md:pl-16 max-h-[400px]'>
                     <img src={work} alt='' className='object-cover' />
                 </div>
             </div>
-            <div className='flex items-center'>
+            <div className='flex flex-col md:flex-row md:items-center'>
                 <p className='w-fit text-7xl font-bold'>200,000+</p>
                 <p className='w-full text-right text-2xl font-bold'>
                     growing startups use Slite as their single source of truth
                 </p>
             </div>
-            <div className='grid grid-cols-6 gap-5 py-10'>
-                <div className='flex bg-gray-100 w-full p-7 h-[150px]'>
+            <div className='grid grid-cols-3 md:grid-cols-6 gap-5 py-10'>
+                <div className='flex bg-gray-100 w-full p-5 md:p-7 h-[80px] md:h-[150px]'>
                     <img
                         src='https://source.unsplash.com/1700x1000?logo'
                         alt=''
                         className='bg-gray-100 w-full object-cover'
                     />
                 </div>
-                <div className='flex bg-gray-100 w-full p-7 h-[150px]'>
+                <div className='flex bg-gray-100 w-full p-5 md:p-7 h-[80px] md:h-[150px]'>
                     <img
                         src='https://source.unsplash.com/1700x1000?logo'
                         alt=''
                         className='bg-gray-100 w-full object-cover'
                     />
                 </div>
-                <div className='flex bg-gray-100 w-full p-7 h-[150px]'>
+                <div className='flex bg-gray-100 w-full p-5 md:p-7 h-[80px] md:h-[150px]'>
                     <img
                         src='https://source.unsplash.com/1700x1000?logo'
                         alt=''
                         className='bg-gray-100 w-full object-cover'
                     />
                 </div>
-                <div className='flex bg-gray-100 w-full p-7 h-[150px]'>
+                <div className='flex bg-gray-100 w-full p-5 md:p-7 h-[80px] md:h-[150px]'>
                     <img
                         src='https://source.unsplash.com/1700x1000?logo'
                         alt=''
                         className='bg-gray-100 w-full object-cover'
                     />
                 </div>
-                <div className='flex bg-gray-100 w-full p-7 h-[150px]'>
+                <div className='flex bg-gray-100 w-full p-5 md:p-7 h-[80px] md:h-[150px]'>
                     <img
                         src='https://source.unsplash.com/1700x1000?logo'
                         alt=''
                         className='bg-gray-100 w-full object-cover'
                     />
                 </div>
-                <div className='flex bg-gray-100 w-full p-7 h-[150px]'>
+                <div className='flex bg-gray-100 w-full p-5 md:p-7 h-[80px] md:h-[150px]'>
                     <img
                         src='https://source.unsplash.com/1700x1000?logo'
                         alt=''
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,14 +2,14 @@ const Nav = () => {
     return (
         <div className='max-w-[1400px] flex items-center justify-between font-semibold w-full px-10 py-5'>
             <div>logo</div>
-            <div className='flex items-center gap-5'>
+            <div className='hidden md:flex items-center gap-5'>
                 <div>Product</div>
                 <div>Solutions</div>
                 <div>Pricing</div>
                 <div>Resources</div>
                 <div>Sign in</div>
             </div>
-            <div className='flex gap-3'>
+            <div className='hidden md:flex gap-3'>
                 <button className='border border-black  px-3 py-1'>
                     Book a demo
                 </button>
